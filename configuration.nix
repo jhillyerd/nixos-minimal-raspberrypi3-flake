8ba70356b4c9139249a2fb1f68d3917ceaecb6c5
James Hillyerd <james@hillyerd.com>
2022-04-02 14:56:12 -0700

    remove demo httpd, brings in too many deps

--- configuration.nix
+++ configuration.nix
@@ -3,13 +3,6 @@
 
   services.openssh.enable = true;
 
-  services.httpd = {
-    enable = true;
-    adminAddr = "user@examaple.com";
-  };
-
-  networking.firewall.allowedTCPPorts = [ 80 ];
-
   users.users.root.openssh.authorizedKeys.keys =
     lib.splitString "\n" (builtins.readFile ./authorized_keys.txt);
 }
